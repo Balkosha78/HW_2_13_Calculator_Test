86af05fe6d1aa73ff8f46f26fa3de348a1a2c556
Алексей <balko-aleksey@yandex.ru>
2023-06-12 16:48:22 +0700

    initial commit

---
+++ src/main/java/pro/sky/hw_2_13_calculator/Hw213CalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_2_13_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw213CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw213CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hw_2_13_calculator/Hw213CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_2_13_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw213CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
